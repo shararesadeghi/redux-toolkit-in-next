243be84d089b8ca8dec1d8edb87c3e91e7180b2e
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-11-28 15:26:53 +0330

    create redux folder

---
+++ redux-toolkit/redux/features/counter/counterSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  counter: 0,
+};
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.counter++;
+    },
+    decrement: (state) => {
+      state.counter--;
+    },
+    increamentByAmount: (state, action) => {
+      state.counter += action.payload;
+    },
+  },
+});
+
+export default counterSlice.reducer;
+export const { increment, decrement, increamentByAmount } =
+  counterSlice.actions;
+
+  export const selectCounter = store =>store=>store.counter.counter;
